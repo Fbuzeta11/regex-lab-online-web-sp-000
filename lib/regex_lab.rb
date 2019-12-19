8d9de37c81295f8d54b781e644607e0680fe3ec6
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-12-19 15:42:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/^[aeiouAEIOU]+\w/) then
+     return true
+   else
+     return false
+   end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"unprecedent complications believing in your self understanding".scan(/\w+un/)
+
 end
 
 def words_five_letters_long(text)
