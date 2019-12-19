f21b0556f60d2f38bb9bedc1743168af65cc1c0c
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2019-12-19 15:34:50 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-"unprecedent complications believing in your self".scan(/\w+un+ing/)
+"unprecedent complications believing in your self understanding".scan(/\w+un+ing/)
 end
 
 def words_five_letters_long(text)
